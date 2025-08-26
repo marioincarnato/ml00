1a3bab069bf6488e2b2ad4a1b2ba17630242b92d
Mario Incarnato <mario.incarnato@cnr.it>
2025-08-26 22:59:05 +0200

    fix

--- 2 - Datasets e data preprocessing/script-01.py
+++ 2 - Datasets e data preprocessing/script-01.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import os
 
-os.chdir("/home/mario/sorgenti/git/training/ml00/2 - Datasets e data preprocessing")
+#os.chdir("/home/mario/sorgenti/git/training/ml00/2 - Datasets e data preprocessing")
+os.chdir("C:/sorgenti/git/training/ml00/2 - Datasets e data preprocessing")
 
 iris = pd.read_csv("data/iris.csv")
 iris.head() #mostra le prime 5 entry del dataset
 #iris.head(10) #mostra le prima 10 entry del dataset
 #iris.tail() #mostra le ultime 5 entry del dataset
+
+print(iris.head())
